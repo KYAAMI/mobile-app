0b0b5eab7d6b61178ba04f405e53429a966a4baf
Chris Z <chriszumbika@gmail.com>
2013-07-07 01:10:00 +0200

    Added Tabs To Literature Activity

---
+++ src/com/kyaami/mobileapp/www/BooksActivity.java
@@ -0,0 +1,11 @@
+package com.kyaami.mobileapp.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BooksActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.books_layout);
+    }
+}
---
+++ src/com/kyaami/mobileapp/www/TagsActivity.java
@@ -0,0 +1,10 @@
+package com.kyaami.mobileapp.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TagsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tags_layout);    }
+}
