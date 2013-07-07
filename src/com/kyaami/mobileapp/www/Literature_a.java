71486cd760ffa41dfc58ac8a7d5cc60385e7b0d3
makhabane <christopher.mipi@gmail.com>
2013-07-07 06:54:30 +0200

    merchandise updates but not fully functioning

--- src/com/kyaami/mobileapp/www/Literature_a.java
+++ src/com/kyaami/mobileapp/www/Literature_a.java
@@ -1,22 +1,44 @@
 package com.kyaami.mobileapp.www;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class Literature_a extends Activity {
+@SuppressWarnings("deprecation")
+public class Literature_a extends TabActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_literature_a);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.literature_a, menu);
-		return true;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_literature_a);
+         
+        TabHost tabHost = getTabHost();
+         
+        // Tab for Books
+        TabSpec bookspec = tabHost.newTabSpec("Books");
+        // setting Title and Icon for the Tab
+        bookspec.setIndicator("Books", getResources().getDrawable(R.drawable.books));
+        Intent photosIntent = new Intent(this, BooksActivity.class);
+        bookspec.setContent(photosIntent);
+         
+        // Tab for Authors
+        TabSpec authorspec = tabHost.newTabSpec("Authors");        
+        authorspec.setIndicator("Authors", getResources().getDrawable(R.drawable.users));
+        Intent songsIntent = new Intent(this, AuthorsActivity.class);
+        authorspec.setContent(songsIntent);
+         
+        // Tab for Tags
+        TabSpec tagspec = tabHost.newTabSpec("Tags");
+        tagspec.setIndicator("Tags", getResources().getDrawable(R.drawable.lit));
+        Intent videosIntent = new Intent(this, TagsActivity.class);
+        tagspec.setContent(videosIntent);
+         
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(bookspec); // Adding Books tab
+        tabHost.addTab(authorspec); // Adding Authors tab
+        tabHost.addTab(tagspec); // Adding Tags tab
+    }
 
 }
--- src/com/kyaami/mobileapp/www/Merchandise_a.java
+++ src/com/kyaami/mobileapp/www/Merchandise_a.java
@@ -1,8 +1,12 @@
 package com.kyaami.mobileapp.www;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class Merchandise_a extends Activity {
 
@@ -10,13 +14,17 @@ public class Merchandise_a extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_merchandise_a);
-	}
+		
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+	    gridview.setAdapter(new ImageAdapter(this));
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.merchandise_a, menu);
-		return true;
+	    gridview.setOnItemClickListener(new OnItemClickListener(){
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(Merchandise_a.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+		
 	}
-
 }
+
+
